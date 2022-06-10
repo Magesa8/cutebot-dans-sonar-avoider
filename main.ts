63f41aaba378f3dfc0fc79d8335f6c5677aaaf78
Magesa8 <allie@autistry.com>
2022-06-09 23:29:39 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (true) {
+        cuteBot.moveTime(cuteBot.Direction.forward, 50, 0.25)
+    }
+    while (cuteBot.ultrasonic(cuteBot.SonarUnit.Inches) <= 10) {
+        cuteBot.moveTime(cuteBot.Direction.backward, 50, 0.25)
+        cuteBot.moveTime(cuteBot.Direction.left, 50, 0.25)
+    }
+})
